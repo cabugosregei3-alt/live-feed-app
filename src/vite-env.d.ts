a8e6514343d23919c3570684510e3bd5047eb4b3
cabugosregei3-alt <cabugosregei3@gmail.com>
2026-01-11 02:15:41 +0800

    Relaxed typescript rules to allow APK build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+// This tells TypeScript how to handle Vue files
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// This prevents the error for your router file
+declare module './router' {
+  const router: any
+  export default router
+}
