8d24521f04f9e09fcc09af4288f34698696676a7
cabugosregei3-alt <cabugosregei3@gmail.com>
2026-01-11 02:44:44 +0800

    2nd build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <--- ADD THIS LINE: Crucial for Capacitor/Android
   plugins: [
     vue(),
     legacy()
